748c1bd41fc9d94b38489d0e568167d1b44b1934
Mateus Campagnolo <Mateus Campagnolo>
2017-02-15 14:12:17 -0200

    Reorganizado HTML e inserido icone

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,57 +1,72 @@
 from app import app
 from app.models.default import User
 from bottle import request, template, static_file, redirect
-from app.models.default import insert_user
 
 
 # static routes
 @app.get('/<filename:re:.*\.css>')
 def stylesheets(filename):
-	return static_file(filename, root='app/static/css')
+    return static_file(filename, root='app/static/css')
+
 
 @app.get('/<filename:re:.*\.js>')
 def javascripts(filename):
-	return static_file(filename, root='app/static/js')
+    return static_file(filename, root='app/static/js')
+
 
 @app.get('/<filename:re:.*\.(jpg|png|gif|ico)>')
 def images(filename):
-	return static_file(filename, root='app/static/img')
+    return static_file(filename, root='app/static/img')
+
 
 @app.get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
 def fonts(filename):
-	return static_file(filename, root='app/static/fonts')
+    return static_file(filename, root='app/static/fonts')
 
 
-@app.route('/') # @get('/')
+@app.route('/')  # @get('/')
 def login():
-	return template('login', sucesso=True)
+    return template('login', sucesso=True)
 
-@app.route('/', method='POST') # @post('/')
+
+@app.route('/', method='POST')  # @post('/')
 def acao_login(db):
-	username = request.forms.get('username')
-	password = request.forms.get('password')
-	result = db.query(User).filter((User.username == username) & (User.password == password)).all()
-	if result:
-		return redirect('/usuarios')
-	else:
-		return template('login', sucesso=False)
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    result = db.query(User).filter((User.username == username) & (User.password == password)).all()
+    if result:
+        return redirect('/usuarios')
+    else:
+        return template('login', sucesso=False)
+
 
 @app.route('/cadastro')
 def cadastro():
-	return template('cadastro')
+    return template('cadastro', existe_username=False)
+
+
 @app.route('/cadastro', method='POST')
 def acao_cadastro(db):
-	username = request.forms.get('username')
-	password = request.forms.get('password')
-	new_user = User(username, password)
-	db.add(new_user)
-	return redirect('/usuarios')
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    try:
+        db.query(User).filter(User.username == username).one()
+        existe_username = True
+    except:
+        existe_username = False
+    if not existe_username:
+        new_user = User(username, password)
+        db.add(new_user)
+        return redirect('/usuarios')
+    return template('cadastro', existe_username=True)
+
 
 @app.route('/usuarios')
 def usuarios(db):
-	usuarios = db.query(User).all()
-	return template('lista_usuarios', usuarios=usuarios)
+    usuarios = db.query(User).all()
+    return template('lista_usuarios', usuarios=usuarios)
+
 
 @app.error(404)
 def error404(error):
-	return template('pagina404')
+    return template('pagina404')
